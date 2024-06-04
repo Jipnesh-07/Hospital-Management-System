e1ae866e62e350b7c369c7a6cba1137a15c52f2b
Jipnesh <jipneshjindal07@gmail.com>
2024-06-04 22:38:46 +0530

    Initial Commit

---
+++ Hospital-Management-System/Hospital_Management_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hospital_Management_SystemApp.swift
+//  Hospital-Management-System
+//
+//  Created by MACBOOK on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Hospital_Management_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
